778e177f1fa1151dba74b1d24057b27bb327f912
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-11-02 18:21:05 -0300

    models cleaning

--- src/project/models.py
+++ src/project/models.py
@@ -1,5 +1,4 @@
 from project import db
-from sqlalchemy.orm import validates
 
 
 class User(db.Model):
@@ -14,31 +13,3 @@ class User(db.Model):
     email = db.Column(db.String(256), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     active = db.Column(db.Boolean, default=True, nullable=False)
-
-    @validates('first_name')
-    def validate_first_name(self, key, first_name):
-        if not first_name:
-            raise ValueError('first_name')
-
-        return first_name
-
-    @validates('last_name')
-    def validate_last_name(self, key, last_name):
-        if not last_name:
-            raise ValueError('last_name')
-
-        return last_name
-
-    @validates('email')
-    def validate_email(self, key, email):
-        if not email:
-            raise ValueError('email')
-
-        return email
-
-    @validates('password')
-    def validate_password(self, key, password):
-        if not password:
-            raise ValueError('password')
-
-        return password
