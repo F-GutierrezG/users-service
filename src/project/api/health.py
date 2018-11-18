da27b9ba420034e84f01db381104fc7bddbff6c1
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-11-18 19:25:12 -0300

    ok -> healthy

--- src/project/api/health.py
+++ src/project/api/health.py
@@ -7,5 +7,5 @@ health_blueprint = Blueprint('health', __name__)
 @health_blueprint.route('/users-service/health', methods=['GET'])
 def health():
     return jsonify({
-        'message': 'ok'
+        'message': 'healthy'
     })
