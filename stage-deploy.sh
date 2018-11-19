09edfd0628ec33d6fe2be1e8681137c316d3def3
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-11-18 22:37:07 -0300

    FIX: CI fails when tests fails

--- stage-deploy.sh
+++ stage-deploy.sh
@@ -13,13 +13,13 @@ ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container rm user
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container rm users-db'
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container rm users-swagger'
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker image rm $REGISTRY_REPO/$USERS:$TAG"
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker image rm $REGISTRY_REPO/$USERS_DB:$TAG"
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker image rm $REGISTRY_REPO/$SWAGGER:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker image rm $REGISTRY_REPO/$USERS"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker image rm $REGISTRY_REPO/$USERS_DB"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker image rm $REGISTRY_REPO/$SWAGGER"
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker run -d -e "POSTGRES_USER=postgres" -e "POSTGRES_PASSWORD=postgres" -p 5433:5432 --name users-db --network users-service-network registry.gitlab.com/gusisoft/onelike/client/users-service/users-db:stage'
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker run -d -e "API_URL=definitions/swagger.yml" -p 8081:8080 --name users-swagger --network users-service-network registry.gitlab.com/gusisoft/onelike/client/users-service/users-swagger:stage'
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker run -d -e "FLASK_ENV=development" -e "FLASK_APP=manage.py" -e "APP_SETTINGS=project.config.DevelopmentConfig" -e "DATABASE_URL=postgres://postgres:postgres@users-db:5432/users" -e "DATABASE_TEST_URL=postgres://postgres:postgres@users-db:5432/users_test" -e "SECRET_KEY=secret_key" -p 5001:5000 --name users --network users-service-network registry.gitlab.com/gusisoft/onelike/client/users-service/users:stage'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -e 'POSTGRES_USER=postgres' -e 'POSTGRES_PASSWORD=postgres' -p 5433:5432 --name users-db --network users-service-network $REGISTRY_REPO/$USERS:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -e 'API_URL=definitions/swagger.yml' -p 8081:8080 --name users-swagger --network users-service-network $REGISTRY_REPO/$USERS:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'DATABASE_URL=postgres://postgres:postgres@users-db:5432/users' -e 'DATABASE_TEST_URL=postgres://postgres:postgres@users-db:5432/users_test' -e 'SECRET_KEY=secret_key' -p 5001:5000 --name users --network users-service-network $REGISTRY_REPO/$USERS"
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect client-network users'
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect client-network users-swagger'
