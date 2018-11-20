3b03e7738144b2d6eefc8c46f0dfb53c0ec94145
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-11-20 18:07:04 -0300

    updated swagger definition

--- swagger/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t swagger -f Dockerfile .
-docker container rm -f users-service-swagger
-docker run -d --name users-service-swagger -p 8081:8080 -e "API_URL=definitions/users-service.yml" --rm swagger
